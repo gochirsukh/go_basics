7260ba6af98c2bb47892dc194eec722e8c5b2337
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2022-12-30 09:09:25 -0800

    switch statement

--- switch/example_switch.go
+++ switch/example_switch.go
@@ -1,3 +1,21 @@
 package main
 
 // https://gobyexample.com/switch
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+
+	fmt.Println("Go runs on")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	}
+	myos := runtime.GOOS
+	fmt.Println("OS is: ", myos)
+}
