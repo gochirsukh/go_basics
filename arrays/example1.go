5d2fdc220d029e24442a2a73a628d1a585f27d99
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2023-01-25 08:10:42 -0600

    File Handling

--- arrays/example1.go
+++ arrays/example1.go
@@ -1,1 +1,15 @@
-placeholder
+package main
+
+import "fmt"
+
+func main() {
+
+	arrayOfIntegers := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arrayOfIntegers)
+	fmt.Println("Access index", arrayOfIntegers[3])
+
+	arrayOfStrings := [3]string{"red", "green", "black"}
+	fmt.Println(arrayOfStrings)
+	fmt.Println("Accessed array of strings", arrayOfStrings[2])
+
+}
